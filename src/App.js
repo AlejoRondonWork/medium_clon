11f41f369e777f4c0c968bd7e1f5f2497760eae1
David Alejandro Rondon Berrio <david.rondon@globant.com>
2024-02-21 09:45:36 -0500

    first steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss'
+import './reset.css'
+import MediumIcon from './components/MediumIcon/MediumIcon'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <div>
+          <p>Love you :D</p>
+          <MediumIcon style={{ width: '120px' }}></MediumIcon>
+        </div>
       </header>
-    </div>
-  );
+    </main>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/MediumIcon/MediumIcon.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import './MediumIcon.css'
+function MediumIcon() {
+  return <img className='medium-icon' src='/images/medium_logo.png' alt='medium logo' />
+}
+
+export default MediumIcon
